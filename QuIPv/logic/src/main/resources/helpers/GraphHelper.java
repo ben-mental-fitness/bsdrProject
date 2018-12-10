63249f86dd94e32d27e8de924a80bbe976020fca
DavidManda <davidmanda1998@gmail.com>
2018-12-10 13:20:11 +0000

    refactored GraphNode and created Graph helper

---
+++ QuIPv/logic/src/main/resources/helpers/GraphHelper.java
@@ -0,0 +1,23 @@
+package helpers;
+
+import net.quipv.logic.Models.Answer;
+import net.quipv.logic.Models.Graph;
+import net.quipv.logic.Models.GraphNode;
+import net.quipv.logic.Models.Project;
+
+import java.util.List;
+
+public class GraphHelper {
+    public static List<GraphNode> constructGraph(Project project){
+        Graph graph = new Graph();
+        List<Answer> answers = project.getAnswers();
+
+        for(Answer answer : answers){
+            String driver = answer.getDriver();
+            List<String> outcomes = answer.getOutcomes();
+
+        }
+
+        return graph;
+    }
+}
